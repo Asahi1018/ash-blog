eb963d1c88098f0bdd01df9b9d1ebe9dff02e18c
asahi <islandsunrise1018@icloud.com>
2022-10-30 05:32:03 +0900

    mq　headerの指定

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,14 +5,14 @@ const Header = () => {
     const openMenu = useMenuOpen();
     const dispatch = useMenuOpDispatch();
     return (
-        <header className={styles.header}>
+        <div className={styles.header}>
             <button className={`${styles.btn} ${openMenu ? styles.open : undefined}`} onClick={() => dispatch()}>
                 <span className={styles.btnSpan}></span>
                 <span className={styles.btnSpan}></span>
                 <span className={styles.btnSpan}></span>
             </button>
             <Link href="/"><h1 className={styles.title}>Ash Web App</h1></Link>
-        </header>
+        </div>
     )
 
 }
