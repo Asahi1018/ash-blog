504f7d322060a8d0cbd23536a12cf8e736fd1e95
asahi <ojimaasahi1018@gmail.com>
2022-10-30 17:55:07 +0900

    バグの修正

---
+++ .history/src/pages/posts/index_20221030163852.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BlogHome from "../../components/BlogHome";
+import { client } from "../../../libs/client";
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
+export default function Post({ blog }) {
+  return <BlogHome blog={blog} />;
+}
